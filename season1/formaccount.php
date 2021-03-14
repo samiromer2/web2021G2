c1fd2091abe9644b46ce81d742727484ad342d0a
Samir Omer <samiromer2@yahoo.com>
2021-03-14 13:01:49 +0200

    Add files via upload

---
+++ season1/formaccount.php
@@ -0,0 +1,42 @@
+<html>
+<title>
+System 1
+</title>
+<h1>Form</h1>
+
+<table border='0'>
+<tr>
+<td>account root</td>
+<td><input name = "accountroot" ></td>
+</tr>
+<tr>
+<td>amount</td>
+<td><input name = "amount"></td>
+</tr>
+<tr>
+<td>from</td>
+<td><input name = "from"></td>
+</tr>
+<tr>
+<td>movement</td>
+<td>
+<select name ="movement">
+<option>+</option>
+<option>-</option>
+</select>
+
+</td>
+</tr>
+<tr>
+<td>date</td>
+<td><input type = "date" name = "dateacc"></td>
+</tr>
+<tr>
+<td></td>
+<td>
+<input type = 'submit' name = "next" value = "next" >
+</td>
+</tr>
+</table>
+
+</html>
